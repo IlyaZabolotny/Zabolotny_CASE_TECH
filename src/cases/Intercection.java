4d91476f42830f92396aceef5bb7fe031ff1faa9
Ilya <picasso.ilya@mail.ru>
2020-10-18 18:18:33 +0300

    Added 10 points test and console input

--- src/cases/Intercection.java
+++ src/cases/Intercection.java
@@ -4,26 +4,27 @@ public class Intercection {
     public Point rayCircleInter(Point O, Point A, Circle C){
         Point D=A.subtract(O);
         Point OC=O.subtract(C.getCenter());
+
         double k1=D.dot(D);
         double k2=2*OC.dot(D);
         double k3=OC.dot(OC)-C.getRadius()*C.getRadius();
         double discriminant=k2*k2-4*k1*k3;
-        System.out.println(discriminant);
+
         if (discriminant==0){
             double t=-k2/(2*k1);
             return new Point(O.getX()+t*D.getX(),O.getY()+t*D.getY());
         } else if (discriminant>0){
             double t1=(-k2+Math.sqrt(discriminant))/(2*k1);
             double t2=(-k2-Math.sqrt(discriminant))/(2*k1);
-            System.out.println(t1);
-            System.out.println(t2);
-            if (t1<t2){
+            if (t1<t2 && t1>0){
                 return new Point(O.getX()+t1*D.getX(),O.getY()+t1*D.getY());
-            } else {
+            }
+            if (t1>t2 && t2>0) {
                 return new Point(O.getX()+t2*D.getX(),O.getY()+t2*D.getY());
             }
         } else {
-            return null;
+            System.out.println("No intersection!");
         }
+        return new Point();
     }
 }
--- src/cases/TestMain.java
+++ src/cases/TestMain.java
@@ -1,12 +1,27 @@
 package cases;
 
+import java.util.Scanner;
+
 public class TestMain {
     public static void main(String[] args) {
+        Point[] arrayInterPoints=new Point[10];
         Point O=new Point(0.0,0.0);
-        Point A=new Point(10.0,0.0);
-        Point Center=new Point(10,0.0);
-        Circle C=new Circle(Center,5.0);
+        System.out.println("Enter coordinates of the circle center: ");
+        Scanner sc=new Scanner(System.in);
+        double Cx=sc.nextDouble();
+        double Cy=sc.nextByte();
+        Point Center=new Point(Cx,Cy);
+        System.out.println("Enter the radius of the circle: ");
+        double radius=sc.nextDouble();
+        Circle C=new Circle(Center,radius);
         Intercection intercection=new Intercection();
-        System.out.println(intercection.rayCircleInter(O,A,C).getX()+" "+intercection.rayCircleInter(O,A,C).getY());
+        for (int i=0;i<10;i++){
+            System.out.println("Enter coordinates of the point: ");
+            double Ax=sc.nextDouble();
+            double Ay=sc.nextDouble();
+            Point A=new Point(Ax,Ay);
+            arrayInterPoints[i]=intercection.rayCircleInter(O,A,C);
+        }
+        System.out.println(arrayInterPoints);
     }
 }
