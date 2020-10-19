05c76081af072ffeb8fd346c1f207a311799f1ae
Ilya <picasso.ilya@mail.ru>
2020-10-19 21:39:58 +0300

    added GUI to this app

---
+++ src/cases/RayCircleIntersection.java
@@ -0,0 +1,11 @@
+package cases;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class RayCircleIntersection extends JPanel{
+    @Override
+    public void paintComponent(Graphics g){
+        this.setBackground(Color.white);
+    }
+}
--- src/cases/TestMain.java
+++ src/cases/TestMain.java
@@ -1,6 +1,8 @@
 package cases;
 
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
 public class TestMain {
     public static void main(String[] args) {
@@ -15,13 +17,24 @@ public class TestMain {
         double radius=sc.nextDouble();
         Circle C=new Circle(Center,radius);
         Intercection intercection=new Intercection();
-        for (int i=0;i<10;i++){
-            System.out.println("Enter coordinates of the point: ");
-            double Ax=sc.nextDouble();
-            double Ay=sc.nextDouble();
-            Point A=new Point(Ax,Ay);
-            arrayInterPoints[i]=intercection.rayCircleInter(O,A,C);
-        }
-        System.out.println(arrayInterPoints);
+        System.out.println("Enter coordinates of the point: ");
+        double Ax=sc.nextDouble();
+        double Ay=sc.nextDouble();
+        Point A=new Point(Ax,Ay);
+        System.out.println(intercection.rayCircleInter(O,A,C).getX()+" "+intercection.rayCircleInter(O,A,C).getY());
+//        for (int i=0;i<10;i++){
+//            System.out.println("Enter coordinates of the point: ");
+//            double Ax=sc.nextDouble();
+//            double Ay=sc.nextDouble();
+//            Point A=new Point(Ax,Ay);
+//            arrayInterPoints[i]=intercection.rayCircleInter(O,A,C);
+//        }
+//        System.out.println(arrayInterPoints);
+        JFrame jf = new JFrame("RayCircleIntersection");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        RayCircleIntersection gui = new RayCircleIntersection();
+        jf.add(gui);
+        jf.setSize(500,500);
+        jf.setVisible(true);
     }
 }
