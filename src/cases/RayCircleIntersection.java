835541c2d0e3d981c7773e60d1941fd25f900683
Ilya <picasso.ilya@mail.ru>
2020-10-19 23:53:07 +0300

    Painted the intersection point and circle

--- src/cases/RayCircleIntersection.java
+++ src/cases/RayCircleIntersection.java
@@ -1,11 +1,46 @@
 package cases;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.*;
 
 
 public class RayCircleIntersection extends JPanel{
+    private ArrayList<Point> interPoints;
     @Override
     public void paintComponent(Graphics g){
-        this.setBackground(Color.white);
+        super.paintComponent(g);
+        draw(g);
     }
+    public void draw(Graphics g){
+        this.setBackground(Color.WHITE);
+        //Размеры JPanel
+        int w=getWidth();
+        int h=getHeight();
+
+        //Отрисовка осей координат
+        g.drawLine(w/2,0,w/2,h);
+        g.drawLine(0,h/2,w,h/2);
+
+        int Ox=w/2;
+        int Oy=h/2;
+        int pointSize=10;
+
+        g.fillOval(Ox-pointSize/2,Oy-pointSize/2,pointSize,pointSize);
+        g.setColor(Color.RED);
+
+        Point O=new Point(0.0,0.0);
+        Point Center=new Point(100,0);
+        Circle C=new Circle(Center,50);
+        g.drawOval((int) Center.getX()+Ox-50,Oy-(int) Center.getY()-50,100,100);
+
+        Intercection intercection = new Intercection();
+
+        Point A=new Point(10,-4);
+        g.fillOval( Ox+(int) intercection.rayCircleInter(O,A,C).getX()-pointSize/2,Oy-(int) intercection.rayCircleInter(O,A,C).getY()-pointSize/2,pointSize,pointSize);
+
+
+
+    }
+
 }
--- src/cases/TestMain.java
+++ src/cases/TestMain.java
@@ -1,33 +1,19 @@
 package cases;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.*;
 import java.awt.*;
 
 public class TestMain {
     public static void main(String[] args) {
-        Point[] arrayInterPoints=new Point[10];
-        Point O=new Point(0.0,0.0);
-        System.out.println("Enter coordinates of the circle center: ");
-        Scanner sc=new Scanner(System.in);
-        double Cx=sc.nextDouble();
-        double Cy=sc.nextByte();
-        Point Center=new Point(Cx,Cy);
-        System.out.println("Enter the radius of the circle: ");
-        double radius=sc.nextDouble();
-        Circle C=new Circle(Center,radius);
-        Intercection intercection=new Intercection();
-        System.out.println("Enter coordinates of the point: ");
-        double Ax=sc.nextDouble();
-        double Ay=sc.nextDouble();
-        Point A=new Point(Ax,Ay);
-        System.out.println(intercection.rayCircleInter(O,A,C).getX()+" "+intercection.rayCircleInter(O,A,C).getY());
+
 //        for (int i=0;i<10;i++){
 //            System.out.println("Enter coordinates of the point: ");
 //            double Ax=sc.nextDouble();
 //            double Ay=sc.nextDouble();
 //            Point A=new Point(Ax,Ay);
-//            arrayInterPoints[i]=intercection.rayCircleInter(O,A,C);
+//            intercection.rayCircleInter(O,A,C);
 //        }
 //        System.out.println(arrayInterPoints);
         JFrame jf = new JFrame("RayCircleIntersection");
