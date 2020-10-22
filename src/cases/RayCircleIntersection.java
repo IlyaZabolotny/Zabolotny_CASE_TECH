6deae0eb2c6cd99f909814f88c63e1c343f204d4
Ilya <picasso.ilya@mail.ru>
2020-10-22 22:09:42 +0300

    add sector of rays-circle intersection in 30 degrees up-down.

--- src/cases/RayCircleIntersection.java
+++ src/cases/RayCircleIntersection.java
@@ -1,7 +1,6 @@
 package cases;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 import javax.swing.*;
 
 
@@ -25,7 +24,7 @@ public class RayCircleIntersection extends JPanel{
         //Параметры экрана
         int Ox=w/2;
         int Oy=h/2;
-        int pointSize=8;
+        int pointSize=6;
 
         Point O=new Point(0.0,0.0);
 
@@ -42,21 +41,65 @@ public class RayCircleIntersection extends JPanel{
 
         Intercection intercection = new Intercection();
         g.setColor(Color.RED);
-
-        int maxX=500;
-        int minX=100;
-        int maxY=500;
-        int minY=100;
-        for (int i=0; i<10;i++){
-            int Ax=(int)(Math.random()*((maxX-minX)+1))+minX;
-            int Ay=(int)(Math.random()*((maxY-minY)+1))+minY;
-            Point A=new Point(Ax,Ay);
-            if (intercection.rayCircleInter(O,A,C).getX()!=0 && intercection.rayCircleInter(O,A,C).getY()!=0){
-                g.fillOval( Ox+(int) intercection.rayCircleInter(O,A,C).getX()-pointSize/2,
-                        Oy-(int) intercection.rayCircleInter(O,A,C).getY()-pointSize/2,
+        //Ввод координат произвольной точки
+        double O1x=250;
+        double O1y=250;
+        Point O1=new Point(O1x,O1y);
+        g.fillOval( Ox+(int) intercection.rayCircleInter(O,O1,C).getX()-pointSize/2,
+                        Oy-(int) intercection.rayCircleInter(O,O1,C).getY()-pointSize/2,
                         pointSize,pointSize);
+        double lengthOO1=Math.sqrt(O1.getX()*O1.getX()+O1.getY()*O1.getY());
+        double cosPhi=O1.getX()/lengthOO1;
+        double sinPhi=O1.getY()/lengthOO1;
+        Point A1 = new Point(O1.getX()+cosPhi,O1.getY()+sinPhi+100*Math.sqrt(3)/3*cosPhi);
+        g.fillOval( Ox+(int) intercection.rayCircleInter(O,A1,C).getX()-pointSize/2,
+                Oy-(int) intercection.rayCircleInter(O,A1,C).getY()-pointSize/2,
+                pointSize,pointSize);
+        Point A2 = new Point(O1.getX()+cosPhi,O1.getY()-sinPhi-100*Math.sqrt(3)/3*cosPhi);
+        g.fillOval( Ox+(int) intercection.rayCircleInter(O,A1,C).getX()-pointSize/2,
+                Oy-(int) intercection.rayCircleInter(O,A1,C).getY()-pointSize/2,
+                pointSize,pointSize);
+        g.fillOval( Ox+(int) intercection.rayCircleInter(O,A2,C).getX()-pointSize/2,
+                Oy-(int) intercection.rayCircleInter(O,A2,C).getY()-pointSize/2,
+                pointSize,pointSize);
+        double A2y=(int) A2.getY();
+        double A1y=(int) A1.getY();
+        if (A2y<A1y){
+            while (A2y<A1.getY()){
+                Point A3 = new Point(A1.getX(),A2y);
+                if (A3.getX()!=0 && A3.getY()!=0){
+                    g.fillOval( Ox+(int) intercection.rayCircleInter(O,A3,C).getX()-pointSize/2,
+                            Oy-(int) intercection.rayCircleInter(O,A3,C).getY()-pointSize/2,
+                            pointSize,pointSize);
+                }
+                A2y++;
             }
+        } else{
+            while (A2y > A1.getY()) {
+                Point A3 = new Point(A1.getX(), A2y);
+                if (A3.getX()!=0 && A3.getY()!=0) {
+                    g.fillOval(Ox + (int) intercection.rayCircleInter(O, A3, C).getX() - pointSize / 2,
+                            Oy - (int) intercection.rayCircleInter(O, A3, C).getY() - pointSize / 2,
+                            pointSize, pointSize);
+                }
+                A2y--;
+                }
         }
+
+//        int maxX=500;
+//        int minX=100;
+//        int maxY=500;
+//        int minY=100;
+//        for (int i=0; i<10;i++){
+//            int Ax=(int)(Math.random()*((maxX-minX)+1))+minX;
+//            int Ay=(int)(Math.random()*((maxY-minY)+1))+minY;
+//            Point A=new Point(Ax,Ay);
+//            if (intercection.rayCircleInter(O,A,C).getX()!=0 && intercection.rayCircleInter(O,A,C).getY()!=0){
+//                g.fillOval( Ox+(int) intercection.rayCircleInter(O,A,C).getX()-pointSize/2,
+//                        Oy-(int) intercection.rayCircleInter(O,A,C).getY()-pointSize/2,
+//                        pointSize,pointSize);
+//            }
+//        }
 
 
 
